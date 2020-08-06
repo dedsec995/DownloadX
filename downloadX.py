d8366eae72e645eccb775a389da61edba929f699
dedsec995 <41211670+dedsec995@users.noreply.github.com>
2020-08-06 17:06:07 +0530

    DownloadX
    The Main Script that is used to move pdf, docs, images, video and more. Just Run this script and you will and never ever ever get the downloads folder clustered. Where you need to find that one file and you just can't get it. :P

---
+++ downloadX.py
@@ -0,0 +1,84 @@
+from watchdog.observers import Observer
+import time
+from watchdog.events import FileSystemEventHandler
+import os
+import json
+
+class MyHandler(FileSystemEventHandler):
+    # i = 1
+    def on_modified(self, event):
+        setups_ext = (".exe",".msi")
+        images_ext = (".png",".jpg",".jpeg",".bmp",".tiff",".gif")
+        docs_ext = (".doc",".docx",".rtf",".ppt",".xlsx",".csv",".pptx",".xls",".file")
+        videos_ext = (".web",".mp4",".avi",".webm",".flv",".mov",".wmv",".mkv")
+        archive_ext = (".zip",".rar",".7z") 
+        temp_ext = (".crdownload",".tmp")
+        for filename in os.listdir(folder_Main):
+            if filename.endswith(temp_ext):
+                time.sleep(10)
+                break    
+            if filename.endswith(".txt"):
+                print("Text File Found")
+                src = os.path.join(folder_Main,filename)
+                new_destination = os.path.join(folder_Text,filename)
+                os.rename(src,new_destination)
+            elif filename.endswith(".pdf"):
+                print("PDF File Found")
+                src = os.path.join(folder_Main,filename)
+                new_destination = os.path.join(folder_PDF,filename)
+                os.rename(src,new_destination)    
+            elif filename.endswith(setups_ext):
+                print("Setup File Found")
+                src = os.path.join(folder_Main,filename)
+                new_destination = os.path.join(folder_Setups,filename)
+                os.rename(src,new_destination)
+            elif filename.endswith(archive_ext):
+                print("Archive File Found")
+                src = os.path.join(folder_Main,filename)
+                new_destination = os.path.join(folder_Archive,filename)
+                os.rename(src,new_destination)
+            elif filename.endswith(docs_ext):
+                print("Doc File Found")
+                src = os.path.join(folder_Main,filename)
+                new_destination = os.path.join(folder_Docs,filename)
+                os.rename(src,new_destination)
+            elif filename.endswith(images_ext):
+                print("Image File Found")
+                src = os.path.join(folder_Main,filename)
+                new_destination = os.path.join(folder_Images,filename)
+                os.rename(src,new_destination)
+            elif filename.endswith(videos_ext):
+                print("Video File Found")
+                src = os.path.join(folder_Main,filename)
+                new_destination = os.path.join(folder_Videos,filename)
+                os.rename(src,new_destination)
+            else:
+                print("Other File Found")
+                src = os.path.join(folder_Main,filename)
+                new_destination = os.path.join(folder_Others,filename)
+                os.rename(src,new_destination)
+
+
+folder_Main = "C:\\Users\\dedsec995\\Downloads\\Download"
+folder_Text = "C:\\Users\\dedsec995\\Downloads\\Text"
+folder_PDF = "C:\\Users\\dedsec995\\Downloads\\PDF"
+folder_Setups = "C:\\Users\\dedsec995\\Downloads\\Setups"
+folder_Archive = "C:\\Users\\dedsec995\\Downloads\\Archive"
+folder_Docs = "C:\\Users\\dedsec995\\Downloads\\Docs"
+folder_Images = "C:\\Users\\dedsec995\\Downloads\\Images"
+folder_Videos = "C:\\Users\\dedsec995\\Downloads\\Videos"
+folder_Others = "C:\\Users\\dedsec995\\Downloads\\Others"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler,folder_Main, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(20)
+        print("Out of sleep")
+        observer.join()
+except KeyboardInterrupt:
+        observer.stop()
+observer.join()
+
